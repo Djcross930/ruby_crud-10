9db41d47354934428b08b28d88c844f19bee0805
Dan Cross <dancross96@gmail.com>
2022-08-15 21:15:40 -0500

    added create action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,14 @@ class UsersController < ApplicationController
     render json: user.as_json
   end
 
+  def create
+    user = User.new
+    user.name = params[:name]
+    user.subscriber = params[:subscriber]
+    user.save
+    render json: user.as_json
+
+  end
+
 
 end
